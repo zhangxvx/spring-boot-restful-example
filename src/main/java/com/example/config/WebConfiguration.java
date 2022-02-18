310cf444e7975fd0c56577eae54acdec992fd014
zhangxv <zhangxv95@163.com>
2022-02-18 22:46:46 +0800

    delete deprecated

--- src/main/java/com/example/config/WebConfiguration.java
+++ src/main/java/com/example/config/WebConfiguration.java
@@ -1,6 +1,5 @@
 package com.example.config;
 
-import com.example.filter.GlobalExceptionFilter;
 import com.example.filter.JwtAuthenticationFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -17,32 +16,12 @@ import java.util.Collections;
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Bean
-    public FilterRegistrationBean<GlobalExceptionFilter> exceptionFilter() {
-        GlobalExceptionFilter exceptionFilter = new GlobalExceptionFilter();
-        FilterRegistrationBean<GlobalExceptionFilter> filterBean = new FilterRegistrationBean<>(exceptionFilter);
-        filterBean.setUrlPatterns(Collections.singleton("/*"));
-        return filterBean;
-    }
-
-    @Bean
     public FilterRegistrationBean<JwtAuthenticationFilter> jwtFilter() {
         JwtAuthenticationFilter jwtFilter = new JwtAuthenticationFilter();
         FilterRegistrationBean<JwtAuthenticationFilter> filterBean = new FilterRegistrationBean<>(jwtFilter);
         filterBean.setUrlPatterns(Collections.singleton("/pro/*"));
         return filterBean;
     }
-
-    /*废弃重写Servlet拦截参数和响应
-    @Bean
-    @Qualifier(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
-    public DispatcherServlet dispatcherServlet() {
-        return new MyDispatcherServlet();
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new GlobalRequestInterceptor());
-    }*/
 
 
     @Override
